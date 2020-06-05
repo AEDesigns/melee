5aae7439569d73820442230be4a4da0013cc2ef0
aedevdesigns@gmail.com <aedevdesigns@gmail.com>
2020-06-05 09:06:37 -0500

    Add filter out a card functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import mtgCardBack from "./images/mtgCardBack.png"
 
 const Card = (props) => {
+
+    const imageOrFallback = props.card.imageUrl || mtgCardBack; 
+    
     return ( 
-        <img key={props.cards.id} src={props.cards.imageUrl} alt={props.cards.name} style={{
-            borderRadius: 10,
-            width: "148.6666666667px",
-            height: "207.3333333333px"
-        }}/>
+        <div style={{display: "inline-block"}}>
+            <img src={imageOrFallback} alt={props.card.name} style={{
+                borderRadius: 10,
+                width: "148.6666666667px",
+                height: "207.3333333333px"
+            }}/>
+            <div style={{textAlign: 'center'}}>
+                {props.card.name}
+                <div>
+                    <button onClick={() => props.addCard(props.card)}>+</button>
+                    {props.removeCard && <button onClick={() => props.removeCard()}>-</button>}
+                </div>
+            </div>
+        </div>
      );
 }
  
--- src/components/CardSubmit.js
+++ src/components/CardSubmit.js
@@ -1,36 +1,58 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import findCard from '../findCard';
 import Card from './Card';
 import '../index.css';
 
 const CardSubmit = () => {
     
-    const [cardText, setCardText] = useState('');
+    const [searchText, setSearchText] = useState('');
     const [mtgCards, setMtgCards] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [deckView, setDeckView] = useState(JSON.parse(localStorage.getItem('deckView')) || []);
+    
+
+    useEffect(() => {
+        localStorage.setItem('deckView', JSON.stringify(deckView, null, 2));
+    }, [deckView]);
+
 
     function submitHandler(e){
         e.preventDefault()
         setIsLoading(true);
-        findCard(cardText).then(res => {
+        findCard(searchText).then(res => {
             setMtgCards(res.cards)
         }).catch(err => {
             console.error(err)
         }).finally(() => {
             setIsLoading(false);
         })
-        //setMtgCards(findCard(cardText))
+        //setMtgCards(findCard(searchText))
     }
+
+    function addCardToDeck(card){
+        return setDeckView([...deckView, card])
+    }
+
+    const removeCard = (clickedCardIndex) => () => {
+        const filterDeckView = deckView.filter((_, currentIndexInLoop) => {
+            return currentIndexInLoop !== clickedCardIndex
+        })
+        setDeckView(filterDeckView)
+       }
     
     return ( 
         <div>
             <form onSubmit={submitHandler}>
-                <input type="text" onChange={(e) => setCardText(e.target.value)} value={cardText} />
+                <input type="text" onChange={(e) => setSearchText(e.target.value)} value={searchText} />
                 <button type="submit">Submit</button>
             </form>
             <div>
                 {isLoading && 'Fetching Results'}
-                {mtgCards.map((card, index) => <Card cards={card} key={card.id}/>)}
+                {mtgCards.map((card, index) => <Card key={card.id} card={card} addCard={addCardToDeck}/>)}
+            </div>
+            <hr/>
+            <div>
+            {deckView.map((card, index) => <Card key={card.id + `:${index}`} card={card} removeCard={removeCard(index)} addCard={addCardToDeck}/>)}
             </div>
         </div>
      );
